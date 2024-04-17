8cb9dfe022fb9f8db04dea6e9ed1ff41b4e47f78
Abubakir <kubanycbekovabubakir@gmail.com>
2024-04-17 17:28:21 +0600

    First commit

---
+++ src/main/java/com/example/axelormessage/AxelorMessageApplication.java
@@ -0,0 +1,13 @@
+package com.example.axelormessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxelorMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxelorMessageApplication.class, args);
+    }
+
+}
